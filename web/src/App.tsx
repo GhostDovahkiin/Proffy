8fd72c7124090d3bb70f4c5f7210ab3cc7b5795f
Pedro Henrique <pedro.sousa@dcx.ufpb.br>
2020-08-03 17:33:16 -0300

    chore: add static routes files

---
+++ web/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './assets/styles/global.css'
+import Landing from './pages/Landing'
+
+function App() {
+  return (
+    <div className="App">
+      <Landing/>
+    </div>
+  );
+}
+
+export default App;
