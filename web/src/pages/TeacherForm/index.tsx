ff17c36385b69664c5bb60d0544fa680a1558ca9
Pedro Henrique <pedro.sousa@dcx.ufpb.br>
2020-08-04 00:47:53 -0300

    feat: base code teacherform

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import PageHeader from '../../components/PageHeader'
 
 function TeacherForm() {
   return (
-    <h1>Professores</h1>
+    <div id="page-teacher-list" className="container">
+      <PageHeader title="Que incrível que você quer dar aulas."/>
+    </div>
   )
 }
 
