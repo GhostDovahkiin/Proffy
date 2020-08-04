bd0d879f056724d5ed099db81501e0c2176811bf
Pedro Henrique <pedro.sousa@dcx.ufpb.br>
2020-08-03 22:15:29 -0300

    feat: Change href html to React Link

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
 import logoImg from '../../assets/images/logo.svg'
 import landingImg from '../../assets/images/landing.svg'
 
@@ -25,15 +25,15 @@ function Landing() {
         />
 
         <div className="buttons-container">
-          <a href="" className="study">
+          <Link to="/estudar" className="study">
             <img src={studyIcon} alt="Estudar"/>
             Estudar
-          </a>
+          </Link>
 
-          <a href="" className="give-classes">
+          <Link to="/dar-aula" className="give-classes">
             <img src={giveClassesIcon} alt="Estudar"/>
             Dar aulas
-          </a>
+          </Link>
         </div>
 
         <span className="total-connections">
