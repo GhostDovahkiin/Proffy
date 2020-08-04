9cfd5375181a81ba82f4e84fe0dbbf9b0daf87f6
Pedro Henrique <pedro.sousa@dcx.ufpb.br>
2020-08-03 22:15:05 -0300

    feat: ensure Routes component is used instead App

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import './assets/styles/global.css'
-import Landing from './pages/Landing'
+import Routes from './routes';
 
 function App() {
   return (
-    <div className="App">
-      <Landing/>
-    </div>
+    <Routes/>
   );
 }
 
