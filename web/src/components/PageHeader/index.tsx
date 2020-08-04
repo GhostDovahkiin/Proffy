bae46760e368b24eff7c5acd3819614aa17d4db2
Pedro Henrique <pedro.sousa@dcx.ufpb.br>
2020-08-03 23:25:29 -0300

    refactor: add interface to header adapts to each page title and form

--- web/src/components/PageHeader/index.tsx
+++ web/src/components/PageHeader/index.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { FC } from 'react'
 import {Link} from 'react-router-dom'
 import backIcon from '../../assets/images/icons/back.svg'
 import logoImg from '../../assets/images/logo.svg'
 
-function PageHeader() {
+interface PageHeaderProps {
+  title: string
+}
+
+const PageHeader:React.FC<PageHeaderProps> = (props) => {
   return (
     <header className="page-header">
       <div className="top-bar-container">
@@ -14,7 +18,8 @@ function PageHeader() {
       </div>
 
       <div className="header-content">
-        <strong>Esses são os Proffys disponíveis.</strong>
+        <strong>{props.title}</strong>
+        {props.children}
       </div>
     </header>
   )
