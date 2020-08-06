9e52cf40bea643616b26227ab5210e69f7436e8a
Pedro Henrique <pedro.sousa@dcx.ufpb.br>
2020-08-06 16:20:27 -0300

    feat: add routes to connections and classes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,14 +1,9 @@
-import express from 'express'
-import database from './database/connection'
-import convertHourToMinutes from './utils/convertHourToMinutes'
-
+import express, { response } from 'express'
+import ClassesController from './controllers/ClassesController'
+import ConnectionsController from './controllers/ConnectionsController'
 const routes = express.Router()
-
-interface ScheduleItem {
-  week_day: number,
-  from: string,
-  to: string
-}
+const classesController = new ClassesController()
+const connectionsController = new ConnectionsController()
 
 routes.get('/', async (req, res) => {
   return res.json({
@@ -20,47 +15,9 @@ routes.get('/', async (req, res) => {
   })
 })
 
-routes.post('/aulas', async (req, res) => {
-  const {
-    name,
-    avatar,
-    whatsapp,
-    bio,
-    subject,
-    cost,
-    schedule
-  } = req.body
-
-  const insertedUsersIds = await database('users').insert({
-    name,
-    avatar,
-    whatsapp,
-    bio,
-  })
-
-  const user_id = insertedUsersIds[0]
-
-  const insertedClassesIds = await database('classes').insert({
-    subject,
-    cost,
-    user_id
-  })
-
-  const class_id = insertedClassesIds[0]
-
-  const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
-    return {
-      class_id,
-      week_day: scheduleItem.week_day,
-      from: convertHourToMinutes(scheduleItem.from),
-      to: convertHourToMinutes(scheduleItem.to)
-    }
-  })
-
-  await database('class_schedule').insert(classSchedule);
-
-  return res.send()
-
-})
+routes.post('/aulas', classesController.create)
+routes.get('/aulas', classesController.index)
+routes.get('/connections', connectionsController.index)
+routes.post('/connections', connectionsController.create)
 
 export default routes
