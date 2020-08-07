f8479dd382fd1f2432eed5cb49fa2730670e69fd
Pedro Henrique <pedro.sousa@dcx.ufpb.br>
2020-08-07 16:11:41 -0300

    refactor: Change input to new Input component

--- web/src/pages/Teachers/index.tsx
+++ web/src/pages/Teachers/index.tsx
@@ -2,35 +2,27 @@ import React from 'react'
 import PageHeader from '../../components/PageHeader'
 import './styles.css'
 import TeacherItem from '../../components/TeacherItem'
+import Input from '../../components/Input'
 
 
 function Teachers() {
   return (
     <div id="page-teacher-list" className="container">
-      <PageHeader title="Esses são os Proffys disponíveis.">
+      <PageHeader
+        title="Esses são os Proffys disponíveis."
+        description="Selecione o Proffy a sua escolha e entre em contato para a conexão acontecer.">
         <form id="search-teachers">
-          <div className="input-block">
-            <label htmlFor="subject">Matéria</label>
-            <input type="text" id="subject"/>
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="week_day">Dia da semana</label>
-            <input type="text" id="week_day"/>
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="time">Hora</label>
-            <input type="text" id="time"/>
-          </div>
+          <Input name="subject" label="Matéria"></Input>
+          <Input name="week_day" label="Dia da semana"></Input>
+          <Input type="time" name="time" label="Hora"></Input>
         </form>
       </PageHeader>
 
       <main>
-        <TeacherItem/>
-        <TeacherItem/>
-        <TeacherItem/>
-        <TeacherItem/>
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
       </main>
     </div>
   )
