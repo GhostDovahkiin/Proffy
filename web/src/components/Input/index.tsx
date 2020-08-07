ad9e6a447cf08c549ef78b1b859a319765ccf7be
Pedro Henrique <pedro.sousa@dcx.ufpb.br>
2020-08-07 16:12:54 -0300

    feat: desacoplate input to a component

---
+++ web/src/components/Input/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './styles.css'
+
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  label: string
+  name: string
+}
+
+const Input: React.FC<InputProps> = ({ label, name, ...rest }) => {
+  return (
+    <div className="input-block">
+      <label htmlFor={name}>{label}</label>
+      <input type="text" id={name} {...rest} />
+    </div>
+  )
+}
+
+export default Input
